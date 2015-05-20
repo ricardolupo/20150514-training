5370be2970f98fe2556cc0c8a6ee4f1203c445a0
ricardolupo <rlupo@getchef.com>
2015-05-20 15:33:05 -0400

    Server spec tests for Setup cookbook

--- cookbooks/setup/test/integration/default/serverspec/default_spec.rb
+++ cookbooks/setup/test/integration/default/serverspec/default_spec.rb
@@ -1,12 +1,2 @@
-require 'spec_helper'
-
-describe 'setup::default' do
-
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
-  end
-
-end
+require 'serverspec'
+set :backend, :exec
---
+++ cookbooks/setup/test/integration/default/serverspec/setup_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe 'vim' do
+  it "is awesome" do
+    expect(true).to eq true
+  end
+
+  it "is installed" do
+      expect(package "vim-common").to be_installed
+    end
+end
+
+describe 'nano' do
+  it "is awesome" do
+    expect(true).to eq true
+  end
+
+  it "is installed" do
+    expect(package "nano").to be_installed
+  end
+end
+
+describe 'emacs' do
+  it "is awesome" do
+    expect(true).to eq true
+  end
+
+  it "is installed" do
+    expect(package "emacs").to be_installed
+  end
+end
+
+describe 'tree' do
+  it 'is awesome' do
+    expect(true).to eq true
+  end
+
+  it 'is installed' do
+    expect(package 'tree').to be_installed
+  end
+end
